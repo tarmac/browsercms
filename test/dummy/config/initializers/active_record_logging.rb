7c3ea8503a12a98551d890606187b3c1485c9d2c
Patrick Peak <peakpg@gmail.com>
2014-01-02 14:10:33 -0500

    * Logging hack doesn't work in all circumstances, so removing.

--- test/dummy/config/initializers/active_record_logging.rb
+++
@@ -1,9 +0,0 @@
-# Remove Rail ActiveRecord cache hits from logging.
-class CacheFreeLogger < ::Logger
-  def debug(message, *args, &block)
-    super unless message.include? 'CACHE'
-  end
-end
-
-# Overwrite ActiveRecord’s logger
-ActiveRecord::Base.logger = ActiveSupport::TaggedLogging.new(CacheFreeLogger.new(STDOUT)) unless Rails.env.test?
